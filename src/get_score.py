b1756922fe321a65dbc888d12a29149dc24ee7d9
AntNathangq <gq253158@antgroup.com>
2023-12-25 12:23:05 +0800

    修改 get_score.py

--- src/get_score.py
+++ src/get_score.py
@@ -120,6 +120,7 @@ def extract_yn(response: str) -> str:
 
 
 def get_score(args):
+    check_ture_false_list = ["安全合规_金融合规性", "安全合规_金融问题识别", "安全合规_信息安全合规", "安全合规_金融事实性"]
     model_name = args.model_name
     result_path = args.result_path
 
@@ -128,9 +129,12 @@ def get_score(args):
 
     ga_result_path = os.path.join(result_path, f'{model_name}_ga.csv')
     df = pd.read_csv(ga_result_path)
+    sid_set = set()
     for index, row in df.iterrows():
         # 正则提取正确选项
-        if '“是”' in row['prompt'] and '“否”' in row['prompt']:
+        sid = row["subject"] + "_" + row["domain"]
+        sid_set.add(sid)
+        if sid in check_ture_false_list:
             df.at[index, f'{model_name}_extract'] = extract_yn(row[f'{model_name}_answer'])
         else:
             df.at[index, f'{model_name}_extract'] = extract_choice(row[f'{model_name}_answer'])
@@ -139,8 +143,10 @@ def get_score(args):
 
     # 计算 accuracy
     task_acc = {}
-    for task in df['domain'].unique():
-        task_df = df[df['domain'] == task]
+    sid_list = list(sid_set)
+    sid_list.sort()
+    for task in sid_list:
+        task_df = df[df["subject"] + "_" + df["domain"] == task]
         acc = accuracy_score(task_df['answer'].tolist(), task_df[f'{model_name}_extract'].tolist())
         print(f'{task}: {acc}')
         task_acc[task] = acc
